5f7ce4b6dfb16204a8d9bbadebe6b5cad9788504
RAFAEL AVELAR LOPEZ <rafael.avelar@bancoazteca.com>
2024-04-04 18:32:13 -0600

    WeatherManager/fetchWeather: Create function

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -10,4 +10,8 @@ import Foundation
 
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=467ea964612c1a637ef1fd00874cdd69&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q\(cityName)"
+    }
 }
