e19913c2df5ff97f2abb6e15220a0c699d8494f4
RAFAEL AVELAR LOPEZ <rafael.avelar@bancoazteca.com>
2024-04-04 18:42:19 -0600

    WeatherManager/performRequest; Initialice function with comments of the work will be doing

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -13,5 +13,13 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a URL.
+        //2. Create a URLSession.
+        //3. Give URLSession a task.
+        //4. Start the task.
     }
 }
